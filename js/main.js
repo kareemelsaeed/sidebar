5cce7723a56deceb8f867cf2aae5558e17d56f8c
kareemelsaeed <engkareemelsaeed@gmail.com>
2022-07-16 18:49:15 +0200

    original

---
+++ js/main.js
@@ -0,0 +1,12 @@
+var sid = document.getElementById('sidebar');
+
+function show(){
+    sid.style.marginLeft = '0'
+}
+
+function hide(){
+    sid.style.marginLeft = '-250px'
+}
+
+console.log("hallo world");
+
